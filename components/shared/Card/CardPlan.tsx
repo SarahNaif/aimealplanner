cd0d7ca527541762ca72161e464cfa201c71f6b2
SarahNaif <sarah.althowebi@gmail.com>
2024-12-25 22:45:36 +0300

    [fix]: add "use client " to CardPlan component

--- components/shared/Card/CardPlan.tsx
+++ components/shared/Card/CardPlan.tsx
@@ -1,5 +1,5 @@
+"use client";
 import { Button } from '@/components/ui/button'
-
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { PlanCredit } from '@/types/types';
 import { SignedIn } from '@clerk/nextjs';
